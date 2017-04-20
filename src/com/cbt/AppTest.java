06ea9f82510c1f30d4de6f2ab835fd01075d5482
mariaefaran <mariaefaran@gmail.com>
2017-04-20 14:09:08 -0400

    Additional changes for practice

--- src/com/cbt/AppTest.java
+++ src/com/cbt/AppTest.java
@@ -1,5 +1,15 @@
 package com.cbt;
 
 public class AppTest {
+	public static void main(String[] args){
+		String message = "Congratulations Graduates!";
+		String message2 = "You made it through the trial and fire of automation!";
+		
+		System.out.println("*****************************YAAAAY!!!**********************YAAAAY!!!");
+		
+		System.out.println(message + " " + message2);
+		
+		System.out.println("I hope this turned out right...");
+	}
 
 }
